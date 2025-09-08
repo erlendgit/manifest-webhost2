11582fc77da6b08ffa4809a253b22c77ecb07a2a
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-09-08 22:48:08 +0300

    Using mariadb only

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 apt update && apt upgrade -y
-apt install nginx python3.13 python3.13-venv -y
-apt install mariadb-server mysql-client libmysqlclient-dev -y
-
+apt install nginx python3.13 python3.13-venv python3.13-dev -y
+apt install mariadb-server mariadb-client libmariadb-dev-compat -y
 
 ufw allow 22
 ufw allow 80
