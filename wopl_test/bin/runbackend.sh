4439f2ed041e1fdf3549977b9b1e71d56e8cfc08
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-09-08 21:55:53 +0300

    switch requirements source directory

--- wopl_test/bin/runbackend.sh
+++ wopl_test/bin/runbackend.sh
@@ -10,7 +10,7 @@ cd $WOPL_TEST_HOME
 rm -rf venv
 python3.13 -m venv venv
 source venv/bin/activate
-pip install -r project/backend2/requirements.prod.txt
+pip install -r project/env/backend/requirements.prod.txt
 
 cd $WOPL_TEST_HOME/project/backend2
 
