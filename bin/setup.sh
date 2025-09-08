97ee27ba92f9b3f1efcfd8faf4af41a878109bca
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-09-08 21:52:39 +0300

    install venv explicitly

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt update && apt upgrade -y
-apt install nginx python3.13 -y
+apt install nginx python3.13 python3.13-venv -y
 apt install mariadb-server mariadb-client -y
 
 ufw allow 22
