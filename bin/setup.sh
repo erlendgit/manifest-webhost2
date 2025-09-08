d8e367bbbdddec1788730131a8feb0d88f8f7bb5
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-09-08 20:58:50 +0300

    Install npm and nodejs

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+apt update && apt upgrade -y
 apt install nginx python3.13 -y
 apt install mariadb-server mariadb-client -y
+apt install nodejs npm -y
 
 ufw allow 22
 ufw allow 80
