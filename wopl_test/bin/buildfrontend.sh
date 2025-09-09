ae35e7caf39a44da8752e8f144fdd72815e5947f
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-09-09 09:44:24 +0300

    rebuildfrontend.sh is for root buildfrontend.sh is for the user

--- wopl_test/bin/buildfrontend.sh
+++ wopl_test/bin/buildfrontend.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ $(whoami) != $WOPL_TEST_PROJECT_USER ]]; then
+  echo "Should run as $WOPL_TEST_PROJECT_USER"
+  exit 2
+fi
+
 cd $WOPL_TEST_HOME/project/frontend
 
 set -o allexport
--- wopl_test/bin/rebuildfrontend.sh
+++ wopl_test/bin/rebuildfrontend.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ $(whoami) != $WOPL_TEST_PROJECT_USER ]]; then
-  echo "Should run as $WOPL_TEST_PROJECT_USER"
-  exit 2
-fi
-
 cd $WOPL_TEST_HOME/project/frontend
 
 rm -rf node_modules
 rm -rf dist
 
 cd $CONFIGURATION_ROOT
-bash wopl_test/bin/buildfrontend.sh
+su wopl_test bash -lc wopl_test/bin/buildfrontend.sh
