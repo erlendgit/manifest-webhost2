2217f01d2e9659eef772f7142ff065c67a3e652e
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-12-30 16:51:55 +0100

    Add ruby and sass

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,11 @@ apt update && apt upgrade -y
 apt install nginx python3.13 python3.13-venv python3.13-dev envsubst -y
 apt install mariadb-server mariadb-client libmariadb-dev-compat -y
 apt install redis -y
+
+# Enable sass compile tools
 apt install npm -y
+apt install ruby-full -y
+gem install sass
 
 ufw allow 22
 ufw allow 80
