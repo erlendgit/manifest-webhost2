57af413160e735b8034828e52f6b71257a5ed0a0
Erlend ter Maat <erlend@erlendtermaat.nl>
2026-01-14 21:27:32 +0100

    Run rebuildenv as project user

--- helliot_srp/bin/rebuildenv.sh
+++ helliot_srp/bin/rebuildenv.sh
@@ -1,31 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ $(whoami) != $HSRP_PROJECT_USER ]]; then
-  echo "Should run as $HSRP_PROJECT_USER"
+if [[ $(whoami) != "root" ]]; then
+  echo "Should run as root"
   exit 2
 fi
 
-cd $HSRP_HOME
+cd $CONFIGURATION_ROOT
 
-if [[ -d venv ]]; then
-  rm -rf venv
-fi
-
-python3.13 -m venv venv
-source venv/bin/activate
-
-cd project
-pip install --upgrade pip
-pip install -r requirements.txt
-
-npm install
-export PATH=$(pwd)/node_modules/.bin/:$PATH
-npm run build
-
-set -o allexport
-source .env
-set +o allexport
-
-cd src
-
-python manage.py collectstatic --noinput
+runuser $HSRP_PROJECT_USER -s /bin/bash -- helliot_srp/etc/rebuildenv.sh
